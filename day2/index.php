615282ed881689c77568d61f6d57acda9d1c55e4
guskarisma <guskarisma@gmail.com>
2020-03-11 06:18:31 +0800

    add day2 of training

---
+++ day2/index.php
@@ -0,0 +1,14 @@
+<?php 
+	//this is a comment
+/*
+	this
+	is
+	also
+	a comment
+*/
+	//standard output
+	echo "agus karisma <br>";
+	print "menjadi web developer <br>";
+	print_r("pada bulan April 2020 <br>");// \
+	var_dump("It's cool!");			//   for debugging
+?>
